30ce1a89fc15a9e26b2656da3fad734eb54b58af
Moritz Weibold <moritz@weibold.cc>
2020-04-10 17:47:59 +0200

    Projekt erstellt und ID's vereinheitlicht

---
+++ Projekt_Code/Trainity/TrainityApp/src/main/java/com/trainity/views/LoginPresenter.java
@@ -0,0 +1,34 @@
+package com.trainity.views;
+
+import com.gluonhq.charm.glisten.animation.BounceInRightTransition;
+import com.gluonhq.charm.glisten.application.MobileApplication;
+import com.gluonhq.charm.glisten.control.AppBar;
+import com.gluonhq.charm.glisten.control.FloatingActionButton;
+import com.gluonhq.charm.glisten.mvc.View;
+import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
+import javafx.fxml.FXML;
+
+public class LoginPresenter {
+
+    @FXML
+    private View login;
+
+    public void initialize() {
+        login.setShowTransitionFactory(BounceInRightTransition::new);
+        
+        FloatingActionButton fab = new FloatingActionButton(MaterialDesignIcon.INFO.text,
+                e -> System.out.println("Info"));
+        fab.showOn(login);
+        
+        login.showingProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue) {
+                AppBar appBar = MobileApplication.getInstance().getAppBar();
+                appBar.setNavIcon(MaterialDesignIcon.MENU.button(e -> 
+                        MobileApplication.getInstance().getDrawer().open()));
+                appBar.setTitleText("Login");
+                appBar.getActionItems().add(MaterialDesignIcon.FAVORITE.button(e -> 
+                        System.out.println("Favorite")));
+            }
+        });
+    }
+}
