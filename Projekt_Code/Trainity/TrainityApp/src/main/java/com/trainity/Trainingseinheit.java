6595cfd1aa406cd53db0d1dc141209b90c3bbe18
Moritz Weibold <moritz@weibold.cc>
2020-04-16 11:41:05 +0200

    Grundlegende Klassen erstellt

---
+++ Projekt_Code/Trainity/TrainityApp/src/main/java/com/trainity/Trainingseinheit.java
@@ -0,0 +1,5 @@
+package com.trainity;
+
+public class Trainingseinheit {
+    
+}
--- Prototyp/Trainity/TrainityApp/src/main/java/com/trainity/views/EigeneTrainingsPresenter.java
+++ Prototyp/Trainity/TrainityApp/src/main/java/com/trainity/views/EigeneTrainingsPresenter.java
@@ -34,8 +34,5 @@ public class EigeneTrainingsPresenter {
                         System.out.println("Favorite")));
             }
         });
-    
 }
-
-
 }
